d61117b2b5fadb04cca3adb3329ac7c0f6e65dae
Noé Falzon <noe@yousician.com>
2016-10-21 09:25:50 +0300

    Added tree traversal utility

--- main.lua
+++ main.lua
@@ -1,13 +1,22 @@
 local parser = require "parser"
 local serpent = require "serpent"
+local utils = require "utils"
+local analyzer = require "analyzer"
 
 local prelude = io.open("prelude.mf"):read("*a")
 local source = io.open("test.mf"):read("*a")
+
+prelude = ""
 
 local success, result = pcall(parser.match, parser, (prelude .. source))
 
 if not success then
 	print(result)
-else
-	print(serpent.block(result, {comment = false}))
+	return
 end
+
+--utils.printAST(result)
+
+analyzer.linkParents(result)
+
+print(serpent.block(result, {comment = false}))
---
+++ utils.lua
@@ -0,0 +1,71 @@
+local nop = function(node) return true end
+
+local function traverse(ast, funcTable)
+	
+	if type(ast) ~= "table" then
+		return
+	end
+	
+	funcTable.pre = funcTable.pre or {}
+	funcTable.post = funcTable.post or {}
+	
+	funcTable.pre.default = funcTable.pre.default or nop
+	funcTable.post.default = funcTable.post.default or nop
+	
+	local pre = funcTable.pre[ast.kind] or funcTable.pre.default
+	local continue = pre(ast)
+	
+	if continue then
+		for i,v in ipairs(ast) do
+			traverse(v, funcTable)
+		end
+	end
+	
+	local post = funcTable.post[ast.kind] or funcTable.post.default
+	post(ast)
+end
+
+local function printAST(ast)
+	
+	local ident = 0
+	local identChar = "  "
+
+	local terminal = function(node)
+		print(identChar:rep(ident) .. node.kind .. ": " .. node[1])
+		return false
+	end
+
+	local funcTable =
+	{
+		pre =
+		{
+			default = function(node)
+				print(identChar:rep(ident) .. node.kind .. " (")
+				ident = ident + 1
+				return true
+			end,
+			
+			identifier = terminal,
+			number = terminal
+		},
+		
+		post =
+		{
+			default = function(node)
+				ident = ident - 1
+				print(identChar:rep(ident) .. ")")
+			end,
+			
+			identifier = function() end,
+			number = function() end
+		}
+	}
+
+	traverse(ast, funcTable)
+end
+
+return
+{
+	traverse = traverse,
+	printAST = printAST
+}
