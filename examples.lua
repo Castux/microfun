bb7a2986272827803fa0ad5b46c25a066ef176f3
Noé Falzon <noe@yousician.com>
2017-03-28 16:09:23 +0300

    Split prelude and added examples

---
+++ examples.lua
@@ -0,0 +1,22 @@
+-- Some arithmetics
+
+let
+
+divides = n -> eq 0 . mod n,
+divisors = n -> filter (divides n) < range 1 n,
+digits = reverse . map (flip mod 10) . takeWhile (lt 0) . iterate (flip div 10),
+
+isPrime = n -> range 2 (sqrt n) > none (divides n) > and (gte n 2),
+primes = upfrom 1 > filter isPrime,
+primeDivisors = [
+	1 -> {},
+	n -> let
+		divs = filter (eq 0 . mod n) < range 1 (sqrt n),
+		k = head < drop 1 < concat divs {n}
+	in
+		(k, primeDivisors < div n k)
+],
+
+fibonacci = concat {1,1} (zipWith add fibonacci (tail fibonacci))
+
+in
