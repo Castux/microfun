56f0384d205cb3e367247ea95352aea4e3ab745f
Noé Falzon <1331434+Castux@users.noreply.github.com>
2024-12-04 16:57:01 +0200

    Experimenting with Teal

--- test.lua
+++ test.lua
@@ -1,6 +1,7 @@
 local lex = require "lexer"
 local log = require "log"
 local parse = require "parser2"
+local analyze = require "analyzer2"
 
 local success, tokens = lex "test.mf"
 
@@ -16,7 +17,13 @@ end
 local function printTree(node, indent)
 	indent = indent or 0
 
-	print(string.rep("    ", indent) .. (node.name or node.kind or "*"), node.value or "")
+	local name = node.name
+	if node.kind then
+		name = "tok(" .. node.kind .. ")"
+	end
+	name = name or "*"
+
+	print(string.rep("    ", indent) .. name, node.value or "")
 	for i,v in ipairs(node) do
 		printTree(v, indent + 1)
 	end
@@ -24,5 +31,14 @@ end
 
 
 
+
 local tree = parse(tokens)
+
 printTree(tree)
+
+
+if not tree then
+	return
+end
+
+analyze(tree)
