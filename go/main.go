a782c97077b3e29d229df0de4f6f71fe226e7805
Noé Falzon <1331434+Castux@users.noreply.github.com>
2025-11-28 16:23:25 +0200

    Recursive module loading

--- go/main.go
+++ go/main.go
@@ -1,19 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"maps"
+	"os"
+	"slices"
+	"strings"
+)
+
+func LoadProgram(path string) *Program {
+	tokens := Lex(path)
+	if tokens == nil {
+		os.Exit(1)
+	}
+
+	prog := ParseProgram(tokens)
+	if prog == nil {
+		os.Exit(1)
+	}
+
+	return prog
+}
+
+func LoadModules(imports []*Name) map[string]*Module {
+	loaded := make(map[string]*Module)
+
+	var load func(*Name)
+	load = func(name *Name) {
+		if loaded[name.Value] != nil {
+			return
+		}
+
+		loaded[name.Value] = &Module{}	// recursive guard
+		tokens := Lex(name.Value + ".mf")
+		if tokens == nil {
+			Log("imported here", name.Pos, SeverityInfo)
+			os.Exit(1)
+		}
+
+		module := ParseModule(tokens)
+		if module == nil {
+			Log("imported here", name.Pos, SeverityInfo)
+			os.Exit(1)
+		}
+
+		loaded[name.Value] = module
+		for _,name := range module.Imports {
+			load(name)
+		}
+	}
+
+	for _,name := range imports {
+		load(name)
+	}
+	return loaded
+}
 
 func main() {
-
-	tokens := Lex("prelude.mf")
-	if tokens == nil {
-		return
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: microfun <path>")
+		os.Exit(1)
 	}
 
-	//	prog := ParseProgram(tokens)
-	prog := ParseModule(tokens)
-	if prog == nil {
-		return
-	}
+	path := os.Args[1]
+	program := LoadProgram(path)
+	fmt.Println("Loaded program: " + path)
+
+	modules := LoadModules(program.Imports)
+	fmt.Println("Loaded modules: " + strings.Join(slices.Collect(maps.Keys(modules)), ", "))
 
 	// var b bytes.Buffer
 	// enc := json.NewEncoder(&b)
@@ -23,12 +77,12 @@ func main() {
 	//
 	// fmt.Println(b.String())
 
-	PrintAST(prog)
+	// PrintAST(prog)
 
-	n := 0
-	Traverse(prog, func(node Node) {
-		Log(fmt.Sprintf("%d: %s", n, NodeType(node)), NodePos(node), SeverityInfo)
-		n++
-	}, nil)
+	// n := 0
+	// Traverse(prog, func(node Node) {
+	// 	Log(fmt.Sprintf("%d: %s", n, NodeType(node)), NodePos(node), SeverityInfo)
+	// 	n++
+	// }, nil)
 
 }
